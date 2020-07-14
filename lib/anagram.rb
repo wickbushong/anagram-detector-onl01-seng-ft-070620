08f03eb634a4e1c9f055315be647aba99aa0b2e2
wickbushong <wickbushong@gmail.com>
2020-07-14 10:08:15 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,25 @@
 # Your code goes here!
+require 'pry'
+
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(array)
+        # array.delete_if {|item| item.length != word.length}
+        array = array.map do |instance|
+            # binding.pry
+            if word.split('').sort == instance.split('').sort
+                instance
+            end
+        end
+        array.delete_if {|item| item == nil}
+    end
+
+end
+
+# this = Anagram.new("this")
+# this.match(["sit", "shit", "street"])
